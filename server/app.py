678a59b49e0a280e75ae623075e2657aee49f856
andre_f50 <andre_f50@hotmail.com>
2024-09-02 15:37:29 -0300

    word selector scheduler 1

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
+import random
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 app = Flask(__name__)
@@ -8,17 +10,30 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+data = {'curDay': 0}
 
-def update_days():
+
+def update_word():
+    with open("WORDLIST", "r", encoding='utf-8') as f:
+        lines = f.readlines()
+
+    print()
+
+    selection = random.randint(0, len(lines) - 1)
+    print(lines[selection].split('\n')[0])
+
+    palavra = lines[selection].split(',')[0].strip()
+    dica = lines[selection].split(',')[1].split('\n')[0].strip()
+
+    data['word'] = palavra
+    data['tip'] = dica
     data['curDay'] += 1
-    print(data['curDay'])
 
 
-data = {
-        'palavra': 'PALÍNDROMO',
-        'dica': 'Gramática',
-        'curDay': 3
-        }
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_word, trigger='interval', seconds=60)
+scheduler.start()
+
 
 @app.route('/', methods = ['GET'])
 def index(): 
