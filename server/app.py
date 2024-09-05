def57a3475561998d2212e537329231b728d7512
andre_f50 <andre_f50@hotmail.com>
2024-09-05 10:21:23 -0300

    attempt fix cron 4

--- server/app.py
+++ server/app.py
@@ -32,7 +32,7 @@ def update_word():
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_word, 'cron', hour=7, minute=20)
+scheduler.add_job(update_word, 'cron', hour=13, minute=22)
 scheduler.start()
 
 
