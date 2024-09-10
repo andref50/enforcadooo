4fdddb165cc1913bd3c2d34004d6ebfbb3781659
andre_f50 <andre_f50@hotmail.com>
2024-09-10 18:39:30 -0300

    with statement

--- server/app.py
+++ server/app.py
@@ -11,47 +11,18 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+data = {}
+
 # SQLITE3 DATABASE CONNECTION
 path = os.path.dirname(os.path.abspath(__file__))
 db = os.path.join(path, 'database/wordlist_db')
-conn = sqlite3.connect(db)
-cursor = conn.cursor()
-
-data = {}
 
 # RETRIEVE DATA
-cursor.execute("SELECT * FROM WORDLIST WHERE ativa=True;")
-word_query = cursor.fetchone()
-
-cursor.execute("SELECT * FROM curDay")
-curDay_query = cursor.fetchone()
-
-data['palavra'] = word_query[1]
-data['dica']    = word_query[2]
-data['curDay']  = curDay_query[0]
-
-conn.close()
-
-def update_word():
-    conn = sqlite3.connect(db)
+with sqlite3.connect(db) as conn:
     cursor = conn.cursor()
 
-    # UPDATE curDay AT BEGINNING    
-    cursor.execute("UPDATE curDay SET curDay=curDay + 1")
-    cursor.execute("UPDATE WORDLIST SET ativa=False WHERE ativa=True;")
-
-    cursor.execute("SELECT * FROM usedCount")
-    usedCount_query = cursor.fetchone()
-
-    cursor.execute(f"SELECT * FROM WORDLIST WHERE used={usedCount_query[0]} ORDER BY RANDOM() LIMIT 1")
+    cursor.execute("SELECT * FROM WORDLIST WHERE ativa=True;")
     word_query = cursor.fetchone()
-
-    if not word_query:
-        cursor.execute("UPDATE usedCount SET usedCount=usedCount + 1")
-        cursor.execute("SELECT * FROM usedCount")
-        usedCount_query = cursor.fetchone()
-        cursor.execute(f"SELECT * FROM WORDLIST WHERE used={usedCount_query[0]} ORDER BY RANDOM() LIMIT 1")
-        word_query = cursor.fetchone()
 
     cursor.execute("SELECT * FROM curDay")
     curDay_query = cursor.fetchone()
@@ -60,14 +31,41 @@ def update_word():
     data['dica']    = word_query[2]
     data['curDay']  = curDay_query[0]
 
-    cursor.execute(f"UPDATE WORDLIST SET used={usedCount_query[0]} + 1, ativa=True WHERE ID={word_query[0]}")
+def update_word():
+    with sqlite3.connect(db) as conn:
+        cursor = conn.cursor()
 
-    conn.commit()
-    conn.close()
+        # UPDATE curDay AT BEGINNING    
+        cursor.execute("UPDATE curDay SET curDay=curDay + 1")
+        cursor.execute("UPDATE WORDLIST SET ativa=False WHERE ativa=True;")
+
+        cursor.execute("SELECT * FROM usedCount")
+        usedCount_query = cursor.fetchone()
+
+        cursor.execute(f"SELECT * FROM WORDLIST WHERE used={usedCount_query[0]} ORDER BY RANDOM() LIMIT 1")
+        word_query = cursor.fetchone()
+
+        if not word_query:
+            cursor.execute("UPDATE usedCount SET usedCount=usedCount + 1")
+            cursor.execute("SELECT * FROM usedCount")
+            usedCount_query = cursor.fetchone()
+            cursor.execute(f"SELECT * FROM WORDLIST WHERE used={usedCount_query[0]} ORDER BY RANDOM() LIMIT 1")
+            word_query = cursor.fetchone()
+
+        cursor.execute("SELECT * FROM curDay")
+        curDay_query = cursor.fetchone()
+
+        data['palavra'] = word_query[1]
+        data['dica']    = word_query[2]
+        data['curDay']  = curDay_query[0]
+
+        cursor.execute(f"UPDATE WORDLIST SET used={usedCount_query[0]} + 1, ativa=True WHERE ID={word_query[0]}")
+
+        conn.commit()
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_word, 'interval', seconds=10)
+scheduler.add_job(update_word, 'interval', seconds=30)
 scheduler.start()
 
 
@@ -79,21 +77,31 @@ def index():
     if request.method == 'POST':
         path = os.path.dirname(os.path.abspath(__file__))
         db = os.path.join(path, 'database/wordlist_db')
-        conn = sqlite3.connect(db)
-        cursor = conn.cursor()
+        with sqlite3.connect(db) as conn:
+            cursor = conn.cursor()
 
-        asd = request.json
+            asd = request.json
 
-        if(asd['status'] == 'winner'):
-            cursor.execute(f"UPDATE WORDLIST SET acertos=acertos+1 WHERE ativa=True;")
+            if(asd['status'] == 'winner'):
+                cursor.execute(f"UPDATE WORDLIST SET acertos=acertos+1 WHERE ativa=True;")
 
-        elif(asd['status'] == 'lost'):
-            cursor.execute(f"UPDATE WORDLIST SET erros=erros+1 WHERE ativa=True;")
+            elif(asd['status'] == 'lost'):
+                cursor.execute(f"UPDATE WORDLIST SET erros=erros+1 WHERE ativa=True;")
 
-        conn.commit()
-        conn.close()
+            conn.commit()
 
         return jsonify(asd)
+    
+@app.route('/xyz', methods = ['GET'])
+def actual_status():
+    path = os.path.dirname(os.path.abspath(__file__))
+    db = os.path.join(path, 'database/wordlist_db')
+    with sqlite3.connect(db) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM WORDLIST WHERE ativa=True;")
+        word_query = cursor.fetchone()
+
+    return jsonify(word_query)
 
 
 if __name__ == '__main__':
