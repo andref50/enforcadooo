5863f550291466c1f75120378b33a5abf3035c6a
andre_f50 <andre_f50@hotmail.com>
2024-10-10 14:36:17 -0300

    stats page

---
+++ server/stats.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+app.route('/', methods=['GET'])
+def stats():
+    return '<h1>Oiii</h1>'
+
+if __name__ == '__main__':
+    app.run()
