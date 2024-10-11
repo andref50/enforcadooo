bbf2e0f7dabe49b0cb305556939d61118205ef56
andre_f50 <andre_f50@hotmail.com>
2024-10-11 10:58:55 -0300

    jsonify

--- server/stats.py
+++ server/stats.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 app.route('/', methods=['GET'])
 def stats():
-    return '<h1>Oiii</h1>'
+    return jsonify({'data': 'oiiii'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
