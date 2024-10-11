f54fb80051778035c9a53c4e19ad795b1d789ed3
andre_f50 <andre_f50@hotmail.com>
2024-10-11 11:47:00 -0300

    headers

--- server/stats.py
+++ server/stats.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['CORS_HEADERS'] = 'Content-Type'
-CORS(app, resources={r'/*': {'origins': '*'}})
+# app.config.from_object(__name__)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# CORS(app, resources={r'/*': {'origins': '*'}})
 
 app.route('/', methods=['GET'])
 def stats():
