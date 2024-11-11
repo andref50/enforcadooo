15d8ecbde2594d6c1a768193e1fe8dfea672cf9c
Ubuntu <ubuntu@ip-172-31-43-66.ec2.internal>
2024-11-11 17:14:54 +0000

    stats.py database

--- server/stats.py
+++ server/stats.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+load_dotenv()
+KEY = os.getenv('API_KEY')
+DEV_PROD = os.getenv('DEV_PROD')
+
+print(KEY)
+print(DEV_PROD)
 
 @app.route('/', methods=['GET'])
 def stats():
