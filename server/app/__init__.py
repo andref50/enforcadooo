1bc7fb511e5db54999eac0f42051f8526f2da5ae
Andre Francisco <andre_f50@hotmail.com>
2025-11-01 22:24:38 -0300

    cors

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -9,7 +9,7 @@ from app.models import Data
 app = Flask(__name__)
 app.config.from_object(Config)
 # CORS(app, resources={r'/*': {'origins': '*'}})
-CORS(app)
+CORS(app, origins="http://enforcado.ddns.net:8080")
 
 db = SQLAlchemy(app)
 
